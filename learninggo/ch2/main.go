8a79837e0472cd97af2ce083b42f6dc11c3e862b
Ryan Allen <rallen3882@gmail.com>
2024-12-31 12:03:04 -0500

    chapter 2

---
+++ learninggo/ch2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	ex1()
+	ex2()
+	ex3()
+}
+
+func ex1() {
+	fmt.Println("=== EXERCISE 1 ===")
+
+	var i int = 20
+	var f float64 = float64(i)
+
+	fmt.Println(i)
+	fmt.Println(f)
+}
+
+func ex2() {
+	fmt.Println("=== EXERCISE 2 ===")
+
+	const value = 1
+
+	var i int = value
+	var f float64 = value
+
+	fmt.Println(i)
+	fmt.Println(f)
+}
+
+func ex3() {
+	fmt.Println("=== EXERCISE 3 ===")
+
+	var b byte = 255
+	var smallI int32 = 2147483647
+	var bigI uint64 = 18446744073709551615
+
+	b += 1
+	smallI += 1
+	bigI += 1
+
+	println(b)
+	println(smallI)
+	println(bigI)
+}
